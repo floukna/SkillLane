d0bbb088e2cf6554e4d49f7ba26cbc047b282ef4
Chanapai Suparbpong <chanapaisuparbpong@Chanapais-MacBook-Pro.local>
2021-08-05 12:39:55 +0700

    first commit

--- test1/test.js
+++ test1/test.js
@@ -16,10 +16,11 @@ function countMax(data, max) {
   ).get(max)
 }
 
-function find_largest(floor, n) {
-  let list = [floor * floor]
-  let round = Math.ceil(floor / 2)
-  console.log(round);
+function find_largest(round, n) {
+  let list = [round * round]
+  if (round > 2) {
+    round = Math.ceil(round / 2)
+  }
   let exponent = 3
   while (round > 1) {
     let pow = Math.pow(round, exponent)
@@ -46,13 +47,13 @@ function find_largest(floor, n) {
 function largest_power(n) {
   if (between(n, 2, 4)) {
     return [1, -1]
-  } else if (between(n, 1, 1000000) && n !== 1) {
+  } else if (between(n, 5, 1000000)) {
     const sqrt = Math.sqrt(n)
-    let floor = Math.floor(sqrt)
+    let round = Math.floor(sqrt)
     if (isFloat(sqrt)) {
-      return find_largest(floor, n)
+      return find_largest(round, n)
     } else {
-      return find_largest(floor - 1, n)
+      return find_largest(round - 1, n)
     }
   }
   return [0, -1]
@@ -62,15 +63,15 @@ function largest_power(n) {
 // console.log(largest_power(2187))
 // console.timeEnd('test')
 
-// console.log(largest_power(90))
-// console.log(largest_power(6))
-// console.log(largest_power(65))
-// console.log(largest_power(3))
-// console.log(largest_power(1))
-// console.log(largest_power(81))
-// console.log(largest_power(29))
-// console.log(largest_power(4))
-console.log(largest_power(531445))
+console.log(largest_power(90))
+console.log(largest_power(6))
+console.log(largest_power(65))
+console.log(largest_power(3))
+console.log(largest_power(1))
+console.log(largest_power(81))
+console.log(largest_power(29))
+console.log(largest_power(4))
+// console.log(largest_power(531445))
 // console.log(largest_power(4914))
 // console.log(largest_power(456534))
 // console.log(largest_power(2488325))
